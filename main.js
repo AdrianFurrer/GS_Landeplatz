c0c08d8f66bf8210e3d6412593b199598cbb0227
AdrianFurrer <91180568+AdrianFurrer@users.noreply.github.com>
2023-04-12 09:48:00 +0200

    Verbindung mit Geoserver

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
 import './style.css';
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import Map from 'ol/Map.js';
+import OSM from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
+import TileWMS from 'ol/source/TileWMS.js';
+import View from 'ol/View.js';
 
+const layers = [
+  new TileLayer({
+    source: new OSM(),
+  }),
+  new TileLayer({
+    source: new TileWMS({
+      url: 'http://localhost:8080/geoserver/cite/wms',
+      params: {'LAYERS': 'cite:countries', 'TILED': true},
+      serverType: 'geoserver',
+      // Countries have transparency, so do not fade tiles:
+      transition: 0,
+    }),
+  }),
+];
 const map = new Map({
+  layers: layers,
   target: 'map',
-  layers: [
-    new TileLayer({
-      source: new OSM()
-    })
-  ],
   view: new View({
-    center: [0, 0],
-    zoom: 2
-  })
+    center: [-10997148, 4569099],
+    zoom: 4,
+  }),
 });
