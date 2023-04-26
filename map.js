e80ca3cc316f5e1008ede2234cca116025ec0fb1
liviocadotsch <livio.cadotsch@students.fhnw.ch>
2023-04-26 16:35:08 +0200

    oevstop

--- map.js
+++ map.js
@@ -1,6 +1,7 @@
 // URLs
 
 var geosWMS = 'http://188.227.200.59:8080/geoserver/wms'
+var geosWFS = 'http://188.227.200.59:8080/geoserver/Gleitschirm_LP/ows?service=WFS&'
 
 // OpenLayers TileMap
 
@@ -11,20 +12,38 @@ var swisstopoWMTSSwissimage = 'ch.swisstopo.swissimage'; // Swisstopo WMTS Layer
 var swisstopoWMTSpixel_grau = 'ch.swisstopo.pixelkarte-grau';
 var swisstopoWMTSpixel_farbe ='ch.swisstopo.pixelkarte-farbe';
 var geoserverWFS_Path = 'Gleitschirm_LP:Jura_All_Path_klein';
-var geoserverwfs_Haltestellen
+var geoserverWFS_Haltestellen = 'Gleitschirm_LP:Haltestellen'
 let id_kachel = 9999
 
+
+
+// Source -----------------------------------------------------------------------------
+// Source -----------------------------------------------------------------------------
+// Source -----------------------------------------------------------------------------
 // Source -----------------------------------------------------------------------------
 
 // Tile Analysis
-var sourceKacheln = new ol.source.Vector({
+// WFS
+var sourceKachelnWFS = new ol.source.Vector({
 	format: new ol.format.GeoJSON(),
 	url: function (extent) {
-		return 'http://188.227.200.59:8080/geoserver/Gleitschirm_LP/ows?service=WFS&' +
+		return geosWFS +
 			'version=1.1.0&request=GetFeature&typename=' + geoserverWFS_Jura_Norm_All_klein +
 			'&outputFormat=application/json';
 	},
-	strategy: ol.loadingstrategy.bbox
+	strategy: ol.loadingstrategy.all
+});
+
+//WMS
+var sourceKachelnWMS = new ol.source.TileWMS({
+	url: geosWMS,
+	params: {
+	  'LAYERS': 'Gleitschirm_LP:Jura_Norm_All_klein', 
+	  'TILED': true,
+	  'STYLES': "KachelnWMS",
+  },
+	serverType: 'geoserver',
+	transition: 0,
 });
 
 // Airspace
@@ -32,7 +51,7 @@ var sourceKacheln = new ol.source.Vector({
 var sourceAirspaceWFS = new ol.source.Vector({
 	format: new ol.format.GeoJSON(),
 	url: function (extent) {
-		return 'http://188.227.200.59:8080/geoserver/Gleitschirm_LP/ows?service=WFS&' +
+		return geosWFS +
 			'version=1.1.0&request=GetFeature&typename=' + geoserverWFS_Airspace +
 			'&outputFormat=application/json';
 	},
@@ -52,17 +71,32 @@ var sourceAirspaceWMS = new ol.source.TileWMS({
   });
 
 // Path from LP to OeV
+//WFS
 var sourcePath = new ol.source.Vector({
 	format: new ol.format.GeoJSON(),
 	url: function (extent) {
-		return 'http://188.227.200.59:8080/geoserver/Gleitschirm_LP/ows?service=WFS&' +
+		return geosWFS +
 			'version=1.1.0&cql_filter=id=' + id_kachel + '&request=GetFeature&typename=' + geoserverWFS_Path +
 			'&outputFormat=application/json';
 	},
-	strategy: ol.loadingstrategy.bbox
+	strategy: ol.loadingstrategy.all
 });
 
+// OeV stops
+
+var sourveOeVstopsWFS = new ol.source.Vector({
+	format: new ol.format.GeoJSON(),
+	url: function (extent) {
+		return geosWFS +
+		'version=1.1.0&request=GetFeature&typename=' + geoserverWFS_Haltestellen +
+		'&outputFormat=application/json'
+		
+	},
+	strategy: ol.loadingstrategy.all
+})
+
 // Railways
+//WMS
 var sourceRailwaysWMS = new ol.source.TileWMS({
 	url: geosWMS,
 	params: {
@@ -75,23 +109,132 @@ var sourceRailwaysWMS = new ol.source.TileWMS({
 	});
 
 // Style ----------------------------------------------------------------------------
-var red_stroke = new ol.style.Style({
+var pathstyle = new ol.style.Style({
 	stroke: new ol.style.Stroke({
-		color: 'rgba(255, 255, 0)', // Rot mit 50% Transparenz
-		width: 5
+		color: 'rgba(255, 255, 0)', // yellow
+		width: 2
 	}),
-})
+});
+
+// var pointstyle = new ol.style.Style({
+// 	image: new ol.style.Circle({
+// 	  radius: 5,
+// 	  fill: new ol.style.Fill({
+// 		color: 'rgba(255, 0, 0, 0.7)', // red with opacity 0.7
+// 	  }),
+// 	  stroke: new ol.style.Stroke({
+// 		color: 'rgba(255, 255, 255, 1)', // white with opacity 1
+// 		width: 2,
+// 	  }),
+// 	}),
+//   });
+
+var pointstyle = function(feature, resolution) {
+	var verkehrsmittel_code = feature.get('verkehrsmittel_code');
+	var feature_type = feature.get('feature_type');
+	
+	if ((verkehrsmittel_code === 'A' || verkehrsmittel_code === 'C' || verkehrsmittel_code === 'D') && resolution < 10) {
+	  return new ol.style.Style({
+		image: new ol.style.Circle({
+		  radius: 5,
+		  fill: new ol.style.Fill({
+			color: 'rgba(0, 255, 0, 0.7)', // green with opacity 0.7
+		  }),
+		  stroke: new ol.style.Stroke({
+			color: 'rgba(255, 255, 255, 1)', // white with opacity 1
+			width: 2,
+		  }),
+		}),
+	  });
+	} else if (verkehrsmittel_code === 'B'&& resolution < 30) {
+	  return new ol.style.Style({
+		image: new ol.style.Circle({
+		  radius: 5,
+		  fill: new ol.style.Fill({
+			color: 'rgba(255, 0, 0, 0.7)', // red with opacity 0.7
+		  }),
+		  stroke: new ol.style.Stroke({
+			color: 'rgba(255, 255, 255, 1)', // white with opacity 1
+			width: 2,
+		  }),
+		}),
+	  });
+	} else if (verkehrsmittel_code === 'I' && resolution < 30) {
+	  return new ol.style.Style({
+		image: new ol.style.Circle({
+		  radius: 5,
+		  fill: new ol.style.Fill({
+			color: 'rgba(173, 216, 230, 0.7)', // light blue with opacity 0.7
+		  }),
+		  stroke: new ol.style.Stroke({
+			color: 'rgba(255, 255, 255, 1)', // white with opacity 1
+			width: 2,
+		  }),
+		}),
+	  });
+	} else {
+		return null;
+	}
+  };
+  
+  
+  
 
 // Layer -----------------------------------------------------------------------------
+// Layer -----------------------------------------------------------------------------
+// Layer -----------------------------------------------------------------------------
+// Layer -----------------------------------------------------------------------------
+
+
+// var select = new ol.interaction.Select({
+// 	style: pointstyle,
+// 	layers: [layerWFSOeVstops], // replace with your vector layer
+//   });
+  
+//   map.addInteraction(select);
+  
+//   var popup = new ol.Overlay({
+// 	element: document.getElementById('popup'),
+// 	positioning: 'bottom-center',
+// 	offset: [0, -10],
+//   });
+  
+//   map.addOverlay(popup);
+  
+//   map.on('click', function(evt) {
+// 	var feature = map.forEachFeatureAtPixel(evt.pixel, function(feature) {
+// 	  return feature;
+// 	});
+  
+// 	if (feature) {
+// 	  var name = feature.get('name'); // replace with the actual attribute name that contains the name
+// 	  var coordinate = evt.coordinate;
+// 	  popup.setPosition(coordinate);
+// 	  popup.getElement().innerHTML = name;
+// 	} else {
+// 	  popup.setPosition(undefined);
+// 	}
+//   });
+  
+
+var layerWFSOeVstops = new ol.layer.Vector({
+	source: sourveOeVstopsWFS,
+	style: pointstyle,
+})
+
+var layerWMSKacheln = new ol.layer.Tile({
+	source: sourceKachelnWMS
+});
 
 var layerWMSRailways = new ol.layer.Tile({
 	visible: true,
 	source: sourceRailwaysWMS
-})
+});
 
 var layerPath = new ol.layer.Vector({
 	source: sourcePath,
-	style: red_stroke,
+	style: pathstyle,
+	minZoom: 13.5,
 });
 
 var layerWMSAirspace = new ol.layer.Tile({
@@ -111,8 +254,8 @@ var layerWFSAirspace = new ol.layer.Vector({
 	})
 });
 
-var layerKacheln = new ol.layer.Vector({
-	source: sourceKacheln,
+var layerWFSKacheln = new ol.layer.Vector({
+	source: sourceKachelnWFS,
 	minZoom: 13.5,
 	style: function (feature) {
 		var norm_all = feature.get('norm_all');
@@ -243,34 +386,75 @@ var windLayer = new ol.layer.Vector({
 	})
   
 // Map -------------------------------------------------------------------------
+// Map -------------------------------------------------------------------------
+// Map -------------------------------------------------------------------------
+// Map -------------------------------------------------------------------------
 // The layer order corresponds to the order of presentation.
+
+// const view = new ol.View({
+// 	center: [0, 0],
+// 	zoom: 1,
+//   });
+
 var map = new ol.Map({
 	target: 'map',
 	layers: [
 		wmtsSwissimage, 
 		wmtsPixel_grau, 
-		wmtsPixel_farbe,
-		
-		layerKacheln, 
+		wmtsPixel_farbe, 
+		//layerWMSKacheln,
+		layerWFSKacheln,
 		layerWMSAirspace,
 		layerPath,
 		layerWMSRailways,
 		windLayer,
+		layerWFSOeVstops,
 	],
+	// view: view
 	view: new ol.View({
 		center: ol.proj.fromLonLat([7.81542463758229, 47.351794485583724]),
 		zoom: 14
 	})
 });
 
+
+
 // Click Function --------------------------------------------------------------
 map.on('click', function (e) {
 	map.forEachFeatureAtPixel(e.pixel, function (feature, layer) {
 		console.log(feature.getProperties().id);
 		id_kachel = feature.getProperties().id;
-		vectorPath.getSource().refresh();
+		layerPath.getSource().refresh();
 	});
 });
+
+// Click Function WMS --------------------------------------------------------------
+
+// map.on('click', function (e) {
+
+// 	const viewResolution = /** @type {number} */ (view.getResolution());
+//     // var viewResolution = map.getView().getResolution();
+//     var coordinate = ol.proj.transform(e.coordinate, 'EPSG:3857', 'EPSG:4326');
+//     var url = sourceKachelnWMS.getFeatureInfoUrl(
+//         coordinate,
+//         viewResolution,
+//         'EPSG:4326',
+//         {'INFO_FORMAT': 'application/json'}
+//     );
+//     if (url) {
+//         fetch(url)
+//             .then(function (response) {
+//                 return response.json();
+//             })
+//             .then(function (json) {
+//                 console.log(json);
+//             });
+//     }
+// });
+
+
+
+
 
 
 // Buttons ---------------------------------------------------------------------
